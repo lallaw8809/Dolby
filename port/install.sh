78c90b79e95b30b66be1f4d378f385f639cc388b
lallaw8809 <lalbosco55@gmail.com>
2018-01-04 11:16:20 +0530

    Updated the readme and install.sh

--- port/install.sh
+++ port/install.sh
@@ -1,30 +1,33 @@
-   sudo apt-get update
-   sleep 2;
-   sudo apt-get install git-core 
-   sudo apt-get install flex 
-   sudo apt-get install bison 
-   sudo apt-get install gperf 
-   sudo apt-get install libesd0-dev 
-   sudo apt-get install zip 
-   sudo apt-get install libwxgtk2.6-dev 
-   sudo apt-get install zlib1g-dev
-   sudo apt-get install build-essential 
-   sudo apt-get install  
-   sudo apt-get install fakeroot 
-   sudo apt-get install gnupg 
-   sudo apt-get install libsdl1.2-dev 
-   sudo apt-get install squashfs-tools 
-   sudo apt-get install uboot-mkimage 
-   sudo apt-get install expect
-   sudo apt-get install minicom
-   sudo apt-get install lrzsz 
-   sudo apt-get install tftpd-hpa 
-   sudo apt-get install nfs-kernel-server 
-   sudo apt-get install nfs-common 
-   sudo apt-get install portmap 
-   sudo apt-get install patch 
-   sudo apt-get install vim
-   sudo apt-get install u-boot-tools
-   sudo apt-get install uboot-mkimage
-   sudo apt-get install tftp tftpd tftp-hpa xienetd
-   
+echo 'Updating....' ;sleep 2
+sudo apt-get update ;
+echo 'Installing required sofware....' ;sleep 2
+sudo apt-get install git-core
+sudo apt-get install flex
+sudo apt-get install bison
+sudo apt-get install gperf
+sudo apt-get install libesd0-dev
+sudo apt-get install zip
+sudo apt-get install libwxgtk2.6-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install build-essential
+sudo apt-get install fakeroot
+sudo apt-get install gnupg
+sudo apt-get install libsdl1.2-dev
+sudo apt-get install squashfs-tools
+sudo apt-get install uboot-mkimage
+sudo apt-get install expect
+sudo apt-get install minicom
+sudo apt-get install lrzsz
+sudo apt-get install tftpd-hpa
+sudo apt-get install nfs-kernel-server2
+sudo apt-get install nfs-common
+sudo apt-get install portmap
+sudo apt-get install patch
+sudo apt-get install vim
+sudo apt-get install u-boot-tools
+sudo apt-get install uboot-mkimage
+sudo apt-get install tftp tftpd tftp-hpa xienetd
+sudo apt-get install lib32stdc++6
+sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0
+echo 'Installation is done...'
+
